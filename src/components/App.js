ad3f9cf95de2e7067f6de8fbcc6de44d0cbcd876
Yoshinari SOMAKI <5726167+sdmkun@users.noreply.github.com>
2023-09-20 15:49:22 +0900

    reduxのconnectをhooksに置き換えた

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 
 const applyFilter = searchTerm => article =>
   article.title.toLowerCase().includes(searchTerm.toLowerCase());
 
-const App = ({ articles, searchTerm, onSearch }) =>
-  <div>
-    <Search value={searchTerm} onSearch={onSearch}>
-      <p>Search</p>
-    </Search>
+const App = () => {
+  const articles = useSelector((state) => state.articlesState.articles);
+  const searchTerm = useSelector((state) => state.searchState.searchTerm);
+  const dispatch = useDispatch();
+  const onSearch = searchTerm => dispatch({ type: "SEARCH_SET", searchTerm });
+  return (
+    <div>
+      <Search value={searchTerm} onSearch={onSearch}>
+        <p>Search</p>
+      </Search>
 
-    <Articles articles={articles.filter(applyFilter(searchTerm))} />
+      <Articles articles={articles.filter(applyFilter(searchTerm))} />
 
-    <p>Take the journey to learn Redux in <a href={'https://roadtoreact.com/'}>Taming the State in React</a></p>
-  </div>
+      <p>Take the journey to learn Redux in <a href={'https://roadtoreact.com/'}>Taming the State in React</a></p>
+    </div>
+  );
+}
 
 const Search = ({ value, onSearch, children }) =>
   <div>
@@ -36,15 +43,4 @@ const Articles = ({ articles }) =>
 const Article = ({ article }) =>
   <a href={article.url}>{article.title}</a>
 
-// connecting view layer to state layer with react-redux
-
-const mapStateToProps = state => ({
-  articles: state.articlesState.articles,
-  searchTerm: state.searchState.searchTerm,
-});
-
-const mapDispatchToProps = dispatch => ({
-  onSearch: searchTerm => dispatch({ type: 'SEARCH_SET', searchTerm }),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
